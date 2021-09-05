5708d9722361ee31589ef97d424cacd486cdf3ca
justin <justinsalasdev@gmail.com>
2021-09-05 15:50:57 +0800

    added catfile support and terminal color

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,50 @@
 extern crate structopt;
+extern crate colored;
 
 use structopt::StructOpt;
+use colored::*;
+
 
 #[derive(StructOpt)]
 struct Options {
-    message: String
+    #[structopt(default_value = "Meow!")]
+    /// What does the cat say??
+    message: String,
+
+    #[structopt(short = "d", long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    /// Load the cat picture from specified file
+    catfile: Option<std::path::PathBuf>,
 }
 
 fn main() {
     let options = Options::from_args();
     let message = options.message;
-    println!("{}",message);
+
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shoudn't bark like a dog.")
+    }
+
+    let eye = if options.dead {"x"} else {"o"};
+    println!("{}", message.bright_yellow().underline());
+
+    match &options.catfile {
+        Some (path) => {
+            let cat_template = std::fs::read_to_string(path).expect(&format!("could not read file {:?}", path));  
+            let cat_picture = cat_template.replace("{eye}", eye);
+            println!("{}", &cat_picture);
+
+        },
+        None => {
+            println!(" \\");
+            println!(" \\");
+            println!("  /\\_/\\");
+            println!(" ( {eye} {eye} )", eye=eye.red().bold()); // [2]
+            println!(" =( I )=");
+        }
+    }
+   
 }
